acf2d57b30400b802bdbfc99ff24dad40c00b862
高力 <3071730@qq.com>
2016-01-29 11:53:08 +0800

    添加 GLChartData

---
+++ Pod/Classes/Core/GLChartData.m
@@ -0,0 +1,29 @@
+#import "GLChartData.h"
+
+@implementation GLChartData
+
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        self.xValues       = [NSArray array];
+        self.yValues       = [NSArray array];
+        
+        self.min           = 0.0f;
+        self.max           = 0.0f;
+        
+        self.xStep         = 5;
+        self.yStep         = 5;
+        
+        self.lineWidth     = 0.5f;
+        self.gridWidth     = 0.5f;
+        self.gridColor     = @"#DDDDDD";
+        
+        self.labelFontSize = 9.0f;
+        self.labelColor    = @"#999999";
+    }
+    
+    return self;
+}
+
+@end
