5aa1129072f85cc0336a895dc4991866ebfce3fd
高力 <3071730@qq.com>
2016-02-23 10:25:59 +0800

    删除 Main.storyboard

--- Example/GLChart/GLAppDelegate.m
+++ Example/GLChart/GLAppDelegate.m
@@ -1,10 +1,17 @@
 #import "GLAppDelegate.h"
+#import "GLViewController.h"
 
 @implementation GLAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    
+    self.window.backgroundColor    = [UIColor whiteColor];
+    self.window.rootViewController = [[GLViewController alloc] init];
+    
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
