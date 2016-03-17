96d702b6f2d8e061ca074a235799b86d68443be1
高力 <3071730@qq.com>
2016-03-17 10:43:14 +0800

    注释测试用例

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -18,10 +18,10 @@
     [super tearDown];
 }
 
-- (void)testExample
-{
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
-}
+//- (void)testExample
+//{
+//    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+//}
 
 @end
 
