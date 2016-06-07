0e4599c6906f43b84a5aeabf7ec15cc30e9f0642
高力 <3071730@qq.com>
2016-06-07 14:54:23 +0800

    添加 GLChartDotMarker

---
+++ Pod/Classes/Components/GLChartDotMarker.m
@@ -0,0 +1,13 @@
+#import "GLChartDotMarker.h"
+
+@implementation GLChartDotMarker
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
