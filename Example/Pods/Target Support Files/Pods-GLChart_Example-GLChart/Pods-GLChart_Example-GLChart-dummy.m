6d737fef59ed9c9793ec3b0452fbc385ca886264
高力 <3071730@qq.com>
2016-08-19 16:02:57 +0800

    更新演示

--- Example/Pods/Target Support Files/Pods-GLChart_Example-GLChart/Pods-GLChart_Example-GLChart-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GLChart_Example_GLChart : NSObject
-@end
-@implementation PodsDummy_Pods_GLChart_Example_GLChart
-@end
--- Example/Pods/Target Support Files/Pods-GLChart_Tests-GLChart/Pods-GLChart_Tests-GLChart-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_GLChart_Tests_GLChart : NSObject
-@end
-@implementation PodsDummy_Pods_GLChart_Tests_GLChart
-@end
